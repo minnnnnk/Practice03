d319eb842445dc7ba373e142dbc9196a636577f8
mingyu <jubal9153@gmail.com>
2022-04-18 18:29:48 +0900

    2022-04-18

---
+++ src/com/javaex/practice/Ex09.java
@@ -0,0 +1,14 @@
+package com.javaex.practice;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		for (int y =0; y<10; y++) {
+			for (int x= 1; x<=10; x++) {
+				System.out.print((y+x)+"\t");
+			}
+			System.out.println("");
+		}
+	}
+
+}
