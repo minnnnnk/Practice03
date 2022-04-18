05e1b604531644fac49201132229a65b220dbbf9
mingyu <jubal9153@gmail.com>
2022-04-19 07:45:13 +0900

    너무어렵다

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Ex17 {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		int y = 0;
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
-		for(int y = num ; y>=num ; y--) {
-			for (int i = 0; i<y ; i++) {
+		for(y = num ; y<=num; y++) {
+			for (i = 0; i<=y ; i--) {
 			System.out.print("*");
 			}
 		} System.out.println("");
