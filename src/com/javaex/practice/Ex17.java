42921dc046e78bb7f7adf11ca8b53f0f920d27bf
y <y@DESKTOP-DMV4N7N>
2022-04-19 10:08:48 +0900

    Ex17

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class Ex17 {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i = 0;
-		int y = 0;
+		
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
-		for(y = num ; y<=num; y++) {
-			for (i = 0; i<=y ; i--) {
-			System.out.print("*");
-			}
-		} System.out.println("");
+		int x = 0;
+		int y = 0;
+		
+	    for (x=num; x>0; x--) {
+	    	for (y=1; y<=x; y++ ) {
+	    		System.out.print("*");
+	    		
+	    	}
+	    	System.out.println("");
+	    	
+	    }
+		
+		
 		sc.close();
 	}
 }
